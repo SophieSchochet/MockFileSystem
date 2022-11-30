dc37a3d7a23f1527638dbc008106965223f4eb7e
Sophie Schochet <s.a.schochet@wustl.edu>
2022-11-30 13:46:23 -0600

    done with 17

--- Studio17/Studio17/Studio17.cpp
+++ Studio17/Studio17/Studio17.cpp
@@ -11,11 +11,12 @@ int main()
 	SimpleFileSystem sfs = SimpleFileSystem(); 
 	TextFile tf1 = TextFile("tf1.txt");
 	ImageFile tf2 = ImageFile("tf2.img");
-	sfs.addFile("tf1.txt", tf1);
-
-	sfs.createFile("test1.txt");
+	sfs.addFile("tf1.txt", &tf1);
+	sfs.addFile("tf2.img", &tf2);
 	sfs.createFile("test2.img");
 	sfs.createFile("test3");
+	sfs.openFile("tf1.txt");
+	sfs.deleteFile("tf2.img");
 
 }
 
