cd32fc4545c3c33f32fd517ad499bad591ee8f27
Sophie Schochet <s.a.schochet@wustl.edu>
2022-12-07 17:54:44 -0600

    fixed an error

--- SharedCode/AbstractCommand.h
+++ SharedCode/AbstractCommand.h
@@ -1,9 +1,11 @@
 #pragma once
+#include <string>
 
 class AbstractCommand {
-private:
-	virtual ~AbstractCommand() = default;
+//private:
+//	virtual ~AbstractCommand() = default;
 public: 
 	virtual int execute(std::string s) = 0;
 	virtual void displayInfo() = 0;
+	virtual ~AbstractCommand() = default;
 };
--- Studio20/Studio20/Studio20.cpp
+++ Studio20/Studio20/Studio20.cpp
@@ -1,7 +1,6 @@
 // Studio20.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-//#include "../../SharedCode/PasswordProxy.h"
-//#include "../../SharedCode/TextFile.h"
+#include "../../SharedCode/PasswordProxy.h"
+#include "../../SharedCode/TextFile.h"
 using namespace std;
 
 
